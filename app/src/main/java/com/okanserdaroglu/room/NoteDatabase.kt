ae787eb69386d80fd102a84f53b36ee278bac946
okanSerdaroglu <okanserdaroglu2@gmail.com>
2020-02-20 16:27:40 +0300

    roomCallback added in NoteDatabase

--- app/src/main/java/com/okanserdaroglu/room/NoteDatabase.kt
+++ app/src/main/java/com/okanserdaroglu/room/NoteDatabase.kt
@@ -1,15 +1,21 @@
 package com.okanserdaroglu.room
 
 import android.content.Context
+import android.os.AsyncTask
 import androidx.room.Database
 import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+
 
 /** @Database creates a database.
  *  entities is an array of tables in database
  *  version is version number of database. When there is a
  *  change in database version number will going to chance
  */
+
+/** if two threads want to access this method synchronized
+ * does not give permission it for dont creating multiple instances**/
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
@@ -20,17 +26,40 @@ abstract class NoteDatabase : RoomDatabase() {
 
         private var INSTANCE: NoteDatabase? = null
 
-        fun getAppDataBase(context: Context): NoteDatabase? {
+        fun getInstance(context: Context): NoteDatabase? {
             if (INSTANCE == null) {
                 synchronized(NoteDatabase::class) {
                     INSTANCE = databaseBuilder(
                         context.applicationContext,
                         NoteDatabase::class.java,
                         "note_database"
-                    ).build()
+                    ).addCallback(roomCallBack).build()
                 }
             }
             return INSTANCE
+        }
+
+        var roomCallBack: Callback = object : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) { // do something after database has been created
+                INSTANCE?.let { PopulateDbAsyncTask(it).execute() }
+            }
+
+            override fun onOpen(db: SupportSQLiteDatabase) { // do something every time database is open
+            }
+        }
+
+        class PopulateDbAsyncTask(db: NoteDatabase) : AsyncTask<Void, Void, Void>() {
+
+            var noteDao: NoteDao = db.noteDao()
+
+            override fun doInBackground(vararg p0: Void?): Void? {
+                noteDao.insert(Note("Title 1", "Description 1", 1))
+                noteDao.insert(Note("Title 1", "Description 1", 1))
+                noteDao.insert(Note("Title 1", "Description 1", 1))
+                return null
+
+            }
+
         }
 
     }
---
+++ app/src/main/java/com/okanserdaroglu/room/NoteRepository.kt
@@ -0,0 +1,60 @@
+package com.okanserdaroglu.room
+
+import android.app.Application
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+
+class NoteRepository(application: Application) {
+
+    private var allNotes: LiveData<List<Note>>
+    private var noteDao: NoteDao
+
+    init {
+        val database = NoteDatabase.getInstance(application)
+        noteDao = database!!.noteDao()
+        allNotes = noteDao.getAllNotes()
+    }
+
+    fun insert(note: Note) {
+        NoteAsyncTask(noteDao,Operations.INSERT).execute(note)
+    }
+
+    fun update(note: Note) {
+        NoteAsyncTask(noteDao,Operations.UPDATE).execute(note)
+    }
+
+    fun delete(note: Note) {
+        NoteAsyncTask(noteDao,Operations.DELETE).execute(note)
+    }
+
+
+
+    /** all database operations must be in background thread with room */
+    class NoteAsyncTask(private var noteDao: NoteDao, private var operation:Operations) : AsyncTask<Note, Void, Void>() {
+
+        override fun doInBackground(vararg note: Note?): Void? {
+            /** vararg is an array */
+            when (operation) {
+                Operations.INSERT -> {
+                    note[0]?.let { noteDao.insert(it) }
+                }
+                Operations.DELETE -> {
+                    note[0]?.let { noteDao.delete(it) }
+                }
+                Operations.UPDATE -> {
+                    note[0]?.let { noteDao.delete(it) }
+                }
+
+
+            }
+            return null
+        }
+    }
+
+
+
+   enum class Operations {
+       INSERT,UPDATE,DELETE,DELETE_ALL,GET_ALL
+   }
+
+}
