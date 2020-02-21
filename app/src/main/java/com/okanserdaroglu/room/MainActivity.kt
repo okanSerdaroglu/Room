1c5a9f29a52b1880e318d347a75b558e3439ded0
okanSerdaroglu <okanserdaroglu2@gmail.com>
2020-02-21 16:26:22 +0300

    viewModel created in MainActivity

--- app/src/main/java/com/okanserdaroglu/room/MainActivity.kt
+++ app/src/main/java/com/okanserdaroglu/room/MainActivity.kt
@@ -2,11 +2,22 @@ package com.okanserdaroglu.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+        noteViewModel.getAllNotes().observe(this, Observer {
+            // update recyclerView here
+            Toast.makeText(this, "onChanged", Toast.LENGTH_SHORT).show()
+        })
+
     }
 }
