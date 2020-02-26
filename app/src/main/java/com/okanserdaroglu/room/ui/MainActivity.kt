37f04839046a164ba231bfe8b48d7e03df732ce1
okanSerdaroglu <okanserdaroglu2@gmail.com>
2020-02-26 15:59:21 +0300

    fragment_note added

--- app/src/main/java/com/okanserdaroglu/room/ui/MainActivity.kt
+++ app/src/main/java/com/okanserdaroglu/room/ui/MainActivity.kt
@@ -1,33 +1,13 @@
 package com.okanserdaroglu.room.ui
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.okanserdaroglu.room.viewModel.NoteViewModel
 import com.okanserdaroglu.room.R
-import com.okanserdaroglu.room.adapter.NoteAdapter
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /*
-        val recyclerViewNotes = findViewById<RecyclerView>(R.id.recyclerViewNotes)
-        recyclerViewNotes.layoutManager = LinearLayoutManager(this)
-        recyclerViewNotes.setHasFixedSize(true)
-        val adapter = NoteAdapter()
-        recyclerViewNotes.adapter = adapter
-
-        val noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
-        noteViewModel.getAllNotes().observe(this, Observer {
-            adapter.setNotes(it)
-        })*/
 
     }
 }
