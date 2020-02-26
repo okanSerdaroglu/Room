94b3e9b5c1bf4a12f9268021ea112a51cf1bb077
okanSerdaroglu <okanserdaroglu2@gmail.com>
2020-02-26 15:37:36 +0300

    noteViewModel observed in mainFragment

--- app/src/main/java/com/okanserdaroglu/room/ui/MainFragment.kt
+++ app/src/main/java/com/okanserdaroglu/room/ui/MainFragment.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.okanserdaroglu.room.R
+import com.okanserdaroglu.room.adapter.NoteAdapter
+import com.okanserdaroglu.room.viewModel.NoteViewModel
 
 class MainFragment : Fragment() {
 
@@ -14,8 +20,22 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_main,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val recyclerViewNotes = activity?.findViewById<RecyclerView>(R.id.recyclerViewNotes)
+        recyclerViewNotes?.layoutManager = LinearLayoutManager(activity)
+        recyclerViewNotes?.setHasFixedSize(true)
+        val adapter = NoteAdapter()
+        recyclerViewNotes?.adapter = adapter
+
+        val noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+        noteViewModel.getAllNotes().observe(viewLifecycleOwner, Observer {
+            adapter.setNotes(it)
+        })
+
     }
 
 }
