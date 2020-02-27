01f80828778927cfd0d1b314f523a18f453e1d20
okanSerdaroglu <okanserdaroglu2@gmail.com>
2020-02-27 14:13:09 +0300

    intent added in navigation graph

--- app/src/main/java/com/okanserdaroglu/room/ui/MainFragment.kt
+++ app/src/main/java/com/okanserdaroglu/room/ui/MainFragment.kt
@@ -7,13 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.okanserdaroglu.room.R
 import com.okanserdaroglu.room.adapter.NoteAdapter
 import com.okanserdaroglu.room.viewModel.NoteViewModel
+import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(),View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +29,7 @@ class MainFragment : Fragment() {
 
         val recyclerViewNotes = activity?.findViewById<RecyclerView>(R.id.recyclerViewNotes)
         recyclerViewNotes?.layoutManager = LinearLayoutManager(activity)
+        buttonAddNote.setOnClickListener(this)
         recyclerViewNotes?.setHasFixedSize(true)
         val adapter = NoteAdapter()
         recyclerViewNotes?.adapter = adapter
@@ -36,6 +39,11 @@ class MainFragment : Fragment() {
             adapter.setNotes(it)
         })
 
+    }
+
+    override fun onClick(p0: View?) {
+        val action = MainFragmentDirections.actionMainFragmentToAddNoteFragment()
+        NavHostFragment.findNavController(this).navigate(action)
     }
 
 }
